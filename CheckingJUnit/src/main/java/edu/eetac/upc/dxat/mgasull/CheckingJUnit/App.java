f918e5a1b536798ced00f60faaad0ac2a273cef3
JthnB <jthnbatlle@yahoo.com>
2014-10-12 23:17:42 +0200

    print canviat

--- CheckingJUnit/src/main/java/edu/eetac/upc/dxat/mgasull/CheckingJUnit/App.java
+++ CheckingJUnit/src/main/java/edu/eetac/upc/dxat/mgasull/CheckingJUnit/App.java
@@ -10,9 +10,7 @@ public class App
     {
     	ComplexOperations m = new ComplexOperations();
         System.out.println( " World!" );
-        System.out.println("ua");
-        System.out.println("mu!!");
-        System.out.println("cosa");
+        System.out.println("he canviat akest print");
         System.out.println("");
         System.out.println(m.ToDegrees(3.14));
     }
