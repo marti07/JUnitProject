09aa1f0547b7f789bfcda762e163e33fb974dcf6
Marti <Marti@Marti-PC>
2014-10-12 20:59:14 +0200

    Commit to provocar error

--- CheckingJUnit/src/main/java/edu/eetac/upc/dxat/mgasull/CheckingJUnit/App.java
+++ CheckingJUnit/src/main/java/edu/eetac/upc/dxat/mgasull/CheckingJUnit/App.java
@@ -10,7 +10,7 @@ public class App
     {
     	ComplexOperations m = new ComplexOperations();
         System.out.println( "Hello World!" );
-        System.out.println("hhhhhhhh");
+        System.out.println("me cagum deu!!");
         System.out.println(m.ToDegrees(3.14));
     }
 }
