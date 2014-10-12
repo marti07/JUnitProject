ddeabd0c0a324f620ecc38c579292f5f7a43f121
Marti <Marti@Marti-PC>
2014-10-12 21:11:25 +0200

    Commit with branches

--- CheckingJUnit/src/main/java/edu/eetac/upc/dxat/mgasull/CheckingJUnit/App.java
+++ CheckingJUnit/src/main/java/edu/eetac/upc/dxat/mgasull/CheckingJUnit/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
     	ComplexOperations m = new ComplexOperations();
-        System.out.println( "Hello World!" );
+        System.out.println( " World!" );
         System.out.println("ua");
         System.out.println("mu!!");
         System.out.println("cosa");
